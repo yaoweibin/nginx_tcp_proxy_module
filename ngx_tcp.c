2f2b78ccb19ae49e3d7961331fe1f1db15a16404
Fritiof Hedman <fritiof@pin.se>
2013-08-06 09:08:53 +0200

    Added support for both the new interface in 1.5.3 and the older ones

--- ngx_tcp.c
+++ ngx_tcp.c
@@ -473,7 +473,11 @@ ngx_tcp_add_addrs(ngx_conf_t *cf, ngx_tcp_port_t *mport,
         addrs[i].conf.ssl = addr[i].ssl;
 #endif
 
+#if defined(nginx_version) && nginx_version >= 1005003
         len = ngx_sock_ntop(addr[i].sockaddr, addr[i].socklen, buf, NGX_SOCKADDR_STRLEN, 1);
+#else
+        len = ngx_sock_ntop(addr[i].sockaddr, buf, NGX_SOCKADDR_STRLEN, 1);
+#endif
 
         p = ngx_pnalloc(cf->pool, len);
         if (p == NULL) {
