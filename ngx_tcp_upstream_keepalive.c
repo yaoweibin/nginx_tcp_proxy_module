acedd90fea72ad6c994a4423827709808dc5a887
ideal <idealities@gmail.com>
2013-10-17 17:33:14 +0800

    add file

--- ngx_tcp_upstream_keepalive.c
+++ ngx_tcp_upstream_keepalive.c
@@ -1,57 +1,8 @@
 
-/*
- * Copyright (C) Maxim Dounin
- * Copyright (C) Nginx, Inc.
- */
-
-
 #include <ngx_config.h>
 #include <ngx_core.h>
-#include <ngx_http.h>
 #include <ngx_tcp.h>
-
-
-typedef struct {
-    ngx_uint_t                         max_cached;
-
-    ngx_queue_t                        cache;
-    ngx_queue_t                        free;
-
-    ngx_http_upstream_init_pt          original_init_upstream;
-    ngx_http_upstream_init_peer_pt     original_init_peer;
-
-} ngx_http_upstream_keepalive_srv_conf_t;
-
-
-typedef struct {
-    ngx_http_upstream_keepalive_srv_conf_t  *conf;
-
-    ngx_http_upstream_t               *upstream;
-
-    void                              *data;
-
-    ngx_event_get_peer_pt              original_get_peer;
-    ngx_event_free_peer_pt             original_free_peer;
-
-#if (NGX_HTTP_SSL)
-    ngx_event_set_peer_session_pt      original_set_session;
-    ngx_event_save_peer_session_pt     original_save_session;
-#endif
-
-} ngx_http_upstream_keepalive_peer_data_t;
-
-
-typedef struct {
-    ngx_http_upstream_keepalive_srv_conf_t  *conf;
-
-    ngx_queue_t                        queue;
-    ngx_connection_t                  *connection;
-
-    socklen_t                          socklen;
-    u_char                             sockaddr[NGX_SOCKADDRLEN];
-
-} ngx_http_upstream_keepalive_cache_t;
-
+#include <ngx_tcp_upstream_keepalive.h>
 
 static ngx_int_t ngx_http_upstream_init_keepalive_peer(ngx_http_request_t *r,
     ngx_http_upstream_srv_conf_t *us);
---
+++ ngx_tcp_upstream_keepalive.h
@@ -0,0 +1,50 @@
+
+#ifndef _NGX_TCP_UPSTREAM_KEEPALIVE_H_INCLUDED_
+#define _NGX_TCP_UPSTREAM_KEEPALIVE_H_INCLUDED_
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_tcp.h>
+
+typedef struct {
+    ngx_uint_t                         max_cached;
+
+    ngx_queue_t                        cache;
+    ngx_queue_t                        free;
+
+    ngx_tcp_upstream_init_pt           original_init_upstream;
+    ngx_tcp_upstream_init_peer_pt      original_init_peer;
+
+} ngx_tcp_upstream_keepalive_srv_conf_t;
+
+
+typedef struct {
+    ngx_tcp_upstream_keepalive_srv_conf_t  *conf;
+
+    ngx_tcp_upstream_t                *upstream;
+
+    void                              *data;
+
+    ngx_event_get_peer_pt              original_get_peer;
+    ngx_event_free_peer_pt             original_free_peer;
+
+#if (NGX_HTTP_SSL)
+    ngx_event_set_peer_session_pt      original_set_session;
+    ngx_event_save_peer_session_pt     original_save_session;
+#endif
+
+} ngx_tcp_upstream_keepalive_peer_data_t;
+
+
+typedef struct {
+    ngx_http_upstream_keepalive_srv_conf_t  *conf;
+
+    ngx_queue_t                        queue;
+    ngx_connection_t                  *connection;
+
+    socklen_t                          socklen;
+    u_char                             sockaddr[NGX_SOCKADDRLEN];
+
+} ngx_http_upstream_keepalive_cache_t;
+
+#endif
